1e471ba7bdafbfde1678db7a41599513b2f5f3d0
Halil <halil.seran.1135@gmail.com>
2022-09-30 19:54:57 +0300

    timeisup

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import classes from "./NewTodo.module.css";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      //throw error
+      return;
+    }
+    props.onAddTodo(enteredText);
+  };
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <label htmlFor="text">Todo Text</label>
+      <input
+        id="text"
+        placeholder="please enter your Todo"
+        type="text"
+        ref={todoTextInputRef}
+      />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+export default NewTodo;
